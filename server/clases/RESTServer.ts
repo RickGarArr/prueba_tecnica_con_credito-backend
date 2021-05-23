f3d6d09f6d3ec965a6cc7e052f2642ccfc2ec0eb
Ricardo <ricardogarciarreola@gmail.com>
2021-05-22 23:56:42 -0500

    proyecto finalizado

--- server/clases/RESTServer.ts
+++ server/clases/RESTServer.ts
@@ -2,12 +2,14 @@ import Express from 'express';
 
 export default class RESTServer {
 
+    // definir una instancia de la clase para usar patron singleton
     private static _instance: RESTServer;
 
     private _app: Express.Application;
     private _port: number = Number(process.env.PORT) || 5000;
 
     private constructor() {
+        // en el constructor se asigna a app la funcion de express
         this._app = Express();
     }
 
@@ -15,16 +17,16 @@ export default class RESTServer {
     public static get instance() {
         return RESTServer._instance || (RESTServer._instance = new RESTServer());
     }
-
+    // get application
     public get app(): Express.Application {
         return this._app;
     }
-
+    // get port
     public get port(): number {
         return this._port;
     }
 
-    // methods
+    // methods para iniciar, recibe callbak
     public start(callback: Function): void {
         this.app.listen(this._port, callback());
     }
--- server/index.ts
+++ server/index.ts
@@ -6,28 +6,31 @@ import CORS from 'cors';
 // server classes
 import LocalServer from './clases/RESTServer';
 import DBConnection from './clases/DBConnection';
-
+// obtener instancia de la clase server
 const server = LocalServer.instance;
 
+// configurar la app de express para usar json y urlencoded
 server.app.use(json());
 server.app.use(urlencoded({extended: true}));
+// permitir los origenes y credenciales
 server.app.use(CORS({origin: true, credentials: true }));
 
+// importar los endpoints y que app las use
 import routes from './rutas/routes';
 server.app.use('/concredito', routes);
-
+// definir el directorio estatico para el frontend
 server.app.use(express.static(__dirname + '/public'));
-
+//en la ruta base se redireccionará a la pagina del frontend
 server.app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, './public/index.html'));
 });
-
+// culquier otra ruta que no esté registrada, la va a resolver la spa frontend
 server.app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/index.html'));
 });
-
+// iniciar la conexion a la base de datos de mongo
 DBConnection.dbConection();
-
+// iniciar la escuca de peticiones
 server.start(() => {
     console.log('Servidor escuchando en el puerto:', server.port);
 });
